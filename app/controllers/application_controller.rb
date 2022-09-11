667fb089f2f0f8e3a8b652a3675db4cc8d7a7aa7
Camilla Poushyk <camilla.poushyk@gmail.com>
2022-09-11 14:02:53 -0400

    commit tree e808c38fc6824e0d8538578201face5aa4598989

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,25 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
   # add routes
 
+    get '/bakeries' do
+      bakeries = Bakery.all
+      bakeries.to_json
+    end
+
+    get '/bakeries/:id' do
+      bakery = Bakery.find(params[:id])
+      bakery.to_json(include: :baked_goods)
+    end
+
+    get '/baked_goods/by_price' do
+      priced_goods = BakedGood.all.order(price: :desc)
+      priced_goods.to_json
+    end
+
+    get '/baked_goods/most_expensive' do
+      pricey_good = BakedGood.all.order(price: :desc).first
+      pricey_good.to_json
+    end
 end
